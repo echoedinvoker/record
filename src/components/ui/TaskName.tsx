dcfbe78ffeabde9d3c1c8ec0c54bc585f962a28f
Matt Chang <echoedinvoker@gmail.com>
2024-08-23 09:16:43 +0800

    task h3 width adjustment

--- src/components/ui/TaskName.tsx
+++ src/components/ui/TaskName.tsx
@@ -3,10 +3,9 @@ import styled from "styled-components";
 const TaskName = styled.h3`
     letter-spacing: 0.1em;
     font-size: 1.2em;
-    word-wrap: break-word;
-    overflow-wrap: break-word;
-    max-width: 100%;
     margin: 0;
+    max-width: 90%;
+    overflow-wrap: break-word;
   `
 
 export default TaskName
--- src/components/ui/TaskNameContainer.tsx
+++ src/components/ui/TaskNameContainer.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const TaskNameContainer = styled.div`
   display: flex;
   justify-content: flex-start;
-  max-width: 12em;
   align-items: center;
 `
 
